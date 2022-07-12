934df85dad6fb7b414b45dcc6532d45ee847aa51
alpknx <alpknx@gmail.com>
2022-07-12 18:00:17 +0300

    feat(login): routing, initial ui, hooks

---
+++ src/AppRouter/AppRouter.tsx
@@ -0,0 +1,36 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { privateRoutes, publicRoutes } from '../router/routing';
+
+const AppRouter = () => {
+  const isAuth = useTypedSelector((state) => state.auth.isAuth);
+  return isAuth ? (
+    <Routes>
+      {privateRoutes.map((route) => {
+        return (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.element />}
+          />
+        );
+      })}
+      <Route path={'/login'} element={<Navigate replace to='/' />} />
+    </Routes>
+  ) : (
+    <Routes>
+      {publicRoutes.map((route) => {
+        return (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.element />}
+          />
+        );
+      })}
+      <Route path={'/'} element={<Navigate replace to='/login' />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
